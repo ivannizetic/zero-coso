44805851749a6c308b53525beb8b270da0bb659a
ivan nizetic <ivan.nizetic@gmail.com>
2021-04-08 22:33:09 -0300

    cosas

---
+++ functions/index.js
@@ -0,0 +1,46 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors")({ origin: true });
+
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
+
+const app = express();
+app.use(cors);
+
+app.get("/rangers", async (req, res) => {
+  try {
+    const rangersRef = await db.collection("rangers").get();
+    let rangers = [];
+    rangersRef.forEach((doc) => {
+      const rangerData = doc.data();
+      rangers.push({
+        id: doc.id,
+        color: rangerData.color,
+      });
+    });
+    res.json({ rangers });
+  } catch (error) {
+    logErrorToServer(error);
+  }
+});
+
+app.get("/rangers/:color", async (req, res) => {
+  try {
+    const { color } = req.params;
+    const rangersRef = db.collection("rangers").doc();
+
+    await rangersRef.set({
+      color,
+    });
+
+    res.json({ id: rangersRef.id, color, success: true });
+  } catch (error) {
+    logErrorToServer(error);
+  }
+});
+
+const logErrorToServer = (error) => console.log(error);
+
+exports.app = functions.https.onRequest(app);
--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
 
+const API_BASE = "https://us-central1-zero-coso.cloudfunctions.net/app";
 function App() {
-  return <div className="App"></div>;
+  const [rangers, setRangers] = React.useState();
+  const getAllRangers = () => {
+    fetch(`${API_BASE}/rangers`)
+      .then((response) => response.json())
+      .then((data) => setRangers(data.rangers));
+  };
+
+  return (
+    <div className="App">
+      <button className="ranger-button" onClick={getAllRangers}>
+        Refresh Rangers
+      </button>
+      <div className="rangers-container"></div>
+      {rangers &&
+        rangers.map((ranger, i) => {
+          return (
+            <>
+              <pre>
+                <code className="ranger">{JSON.stringify(ranger)}</code>
+              </pre>
+              <br />
+            </>
+          );
+        })}
+    </div>
+  );
 }
 
 export default App;
